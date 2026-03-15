4c96811b7762a5a3c6183ca04184e0ce4ad3d4d4
Professional_89 <samuelsepp2006@gmail.com>
2026-03-15 16:57:10 +0100

    Update just-quizzes.js

--- js/just-quizzes.js
+++ js/just-quizzes.js
@@ -710,7 +710,7 @@ window.quizzes = [
         ]
     },
 
-        {
+     {
         id: "medium-player-quiz-5",
         title: "2026 NFL Free Agency Quiz",
         category: "players",
@@ -722,47 +722,47 @@ window.quizzes = [
                 correct: 3
             },
             {
-                question: "Which team signed Bradley Chubb during the 2026 free agency?",
+                question: "Which team signed Bradley Chubb during the 2026 NFL free agency?",
                 answers: ["Denver Broncos", "Buffalo Bills", "Kansas City Chiefs", "Las Vegas Raiders"],
                 correct: 1
             },
             {
-                question: "Which team signed Rico Dowdle during the 2026 free agency?",
+                question: "Which team signed Rico Dowdle during the 2026 NFL free agency?",
                 answers: ["Carolina Panthers", "Pittsburgh Steelers", "Cleveland Browns", "LA Rams"],
                 correct: 1
             },
             {
-                question: "Which team signed Tua Tagovailoa during the 2026 free agency?",
+                question: "Which team signed Tua Tagovailoa during the 2026 NFL free agency?",
                 answers: ["Las Vegas Raiders", "Pittsburgh Steelers", "Atlanta Falcons", "Minnesota Vikings"],
                 correct: 2
             },
             {
-                question: "Which team signed Mike Evans during the 2026 free agency?",
+                question: "Which team signed Mike Evans during the 2026 NFL free agency?",
                 answers: ["Tampa Bay Buccaneers", "Cleveland Browns", "LA Chargers", "San Francisco 49ers"],
                 correct: 3
             },
             {
-                question: "Which team signed Michael Pittman Jr. during the 2026 free agency?",
+                question: "Which team signed Michael Pittman Jr. during the 2026 NFL free agency?",
                 answers: ["Pittsburgh Steelers", "Las Vegas Raiders", "Indianapolis Colts", "San Francisco 49ers"],
                 correct: 0
             },
             {
-                question: "Which team signed Tyler Linderbaum during the 2026 free agency?",
+                question: "Which team signed Tyler Linderbaum during the 2026 NFL free agency?",
                 answers: ["Las Vegas Raiders", "Pittsburgh Steelers", "Seattle Seahawks", "New England Patriots"],
                 correct: 0
             },
             {
-                question: "Which team signed Marcus Mariota during the 2026 free agency?",
+                question: "Which team signed Marcus Mariota during the 2026 NFL free agency?",
                 answers: ["Atlanta Falcons", "Washington Commanders", "Philadelphia Eagles", "Dallas Cowboys"],
                 correct: 1
             },
             {
-                question: "Which team signed Kenneth Walker during the 2026 free agency?",
+                question: "Which team signed Kenneth Walker during the 2026 NFL free agency?",
                 answers: ["Green Bay Packers", "Seattle Seahawks", "Minnesota Vikings", "Kansas City Chiefs"],
                 correct: 3
             },
             {
-                question: "Which team signed Minkah Fitzpatrick during the 2026 free agency?",
+                question: "Which team signed Minkah Fitzpatrick during the 2026 NFL free agency?",
                 answers: ["Arizona Cardinals", "Baltimore Ravens", "NY Jets", "NY Giants"],
                 correct: 2
             }
